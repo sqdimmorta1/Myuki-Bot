445565d78e8fbb57165814969c83fc5ae7be90b0
sqdimmorta1 <44438276+sqdimmorta1@users.noreply.github.com>
2019-08-13 11:49:15 +0600

    Update prev.js

--- commands/prev.js
+++ commands/prev.js
@@ -1,34 +1,34 @@
 const Discord = require('discord.js');
 const moment = require("moment");
 
-module.exports.run = async (bot, message, args, ops) => {
+module.exports.run = async (bot, message, args) => {
 
-  if(!args[0] || args[0] == 1) {
-  
-    let owner = bot.users.find(u => u.id === `294801596754165772`)
-  
-            message.channel.send("Вы не указали сообщение!");
-  };
-    
-  if(args[0] == 'Привет', 'Ку', 'Хай', 'Hello', 'Hi', 'qq', 'q', 'кью', 'Дарова', 'Дороу', 'Здраствуйте'){
-    
-    let rmsg1 = [
-    'Ку!',
-    'qq',
-    'q',
-    'Дороу',
-    'Хэй!',
-    'Йоу',
-    'Приветики!',
-    'Привет',
-    'Хай',
-    'Ку-ку',
-  ]
+//page 1
 
-  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
-  
-  message.channel.send(rmsg1c);
-    return;
-  };
+if(!args[0] || args[0] == 1) {
 
-}
+  let owner = bot.users.find(u => u.id === `294801596754165772`)
+
+          message.channel.send("вы не указали сообщение!");
+};
+if(args[0] == "Привет"){
+  
+  let rmsg = [
+  'Ку!',
+  'qq',
+  'q',
+  'Дороу',
+  'Хэй!',
+  'Йоу',
+  'Приветики!',
+  'Привет',
+  'Хай',
+  'Ку-ку',
+]
+
+const rmsgc = rmsg1[Math.floor(Math.random() * rmsg.length)]
+
+message.channel.send(rmsgc);
+  return;
+};
+
