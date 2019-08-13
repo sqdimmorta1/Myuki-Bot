46cca00d53f4135fb3ac9753c0d7d673346003b3
sqdimmorta1 <44438276+sqdimmorta1@users.noreply.github.com>
2019-08-13 11:52:56 +0600

    Update prev.js

--- commands/prev.js
+++ commands/prev.js
@@ -3,14 +3,10 @@ const moment = require("moment");
 
 module.exports.run = async (bot, message, args) => {
 
-//page 1
-
 if(!args[0] || args[0] == 1) {
-
-  let owner = bot.users.find(u => u.id === `294801596754165772`)
-
           message.channel.send("вы не указали сообщение!");
 };
+  
 if(args[0] == "Привет"){
   
   let rmsg = [
@@ -31,4 +27,6 @@ const rmsgc = rmsg[Math.floor(Math.random() * rmsg.length)]
 message.channel.send(rmsgc);
   return;
 };
+
+}
 
