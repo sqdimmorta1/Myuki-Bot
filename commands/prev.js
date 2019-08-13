dd05f7172227c8d4c4121744840c5612ce963907
sqdimmorta1 <44438276+sqdimmorta1@users.noreply.github.com>
2019-08-13 12:26:37 +0600

    Update prev.js

--- commands/prev.js
+++ commands/prev.js
@@ -31,6 +31,27 @@ if(args[0] == "Привет"){
 message.channel.send(rmsg1c);
   return;
 };
+  
+if(args[0] == "привет"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
 
 if(args[0] == "Ку"){
   
@@ -219,6 +240,405 @@ if(args[0] == "Кью"){
 
 message.channel.send(rmsg1c);
   return;
+};
+  
+if(args[0] == "Привет"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+  
+  if(args[0] == "Пока"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "пока"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Бай"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "бай"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Гудбай"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "гудбай"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Поки"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "поки"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Досвидания"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "досвидания"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Bye"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "bye"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "Goodbye"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "goodbye"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "bb"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "BB"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "ББ"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
+};
+
+if(args[0] == "бб"){
+
+    let rmsg1 = [
+      'бай',
+      'bb',
+      'b',
+      'bye',
+      'goodbye',
+      'пока(',
+      'До встречи!',
+      'Досвидания',
+      'Пока',
+      'Поки-поки',
+]
+
+const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+return;
 };
 
 };
