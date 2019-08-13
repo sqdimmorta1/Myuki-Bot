1441b6501c941218e08c5db5f4edc780b26c5e17
sqdimmorta1 <44438276+sqdimmorta1@users.noreply.github.com>
2019-08-13 12:18:53 +0600

    Update prev.js

--- commands/prev.js
+++ commands/prev.js
@@ -52,6 +52,174 @@ if(args[0] == "Ку"){
 message.channel.send(rmsg1c);
   return;
 };
+  
+if(args[0] == "Здарова"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+  
+if(args[0] == "Дарова"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+  
+if(args[0] == "Дороу"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+  
+if(args[0] == "йй"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+    
+if(args[0] == "ЙЙ"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+    
+if(args[0] == "QQ"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+    
+if(args[0] == "qq"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
+    
+if(args[0] == "Кью"){
+  
+      let rmsg1 = [
+    'Ку!',
+    'qq',
+    'q',
+    'Дороу',
+    'Хэй!',
+    'Йоу',
+    'Приветики!',
+    'Привет',
+    'Хай',
+    'Ку-ку',
+  ]
+
+  const rmsg1c = rmsg1[Math.floor(Math.random() * rmsg1.length)]
+
+message.channel.send(rmsg1c);
+  return;
+};
 
 };
 
